262b8edec0320534c6a298d70f55170107321caa
Hannyer <57728626+Hannyer@users.noreply.github.com>
2025-06-17 00:01:22 -0600

    update admin user

---
+++ SLN_Reservation/DTO/ReservationDto.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLN_Reservation.DTO
+{
+    public class ReservationDto
+    {
+        public List<ReservationE> reservationList { get; set; }
+    }
+}
